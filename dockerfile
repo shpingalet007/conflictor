c9868b1dc26f2f77400e030852358c4aaeb421e6
Shpingalet007 <pavelvernigorov@gmail.com>
2022-09-16 16:43:53 +0200

    Remove nodejs and npm for tests

--- dockerfile
+++ dockerfile
@@ -2,7 +2,7 @@ FROM alpine:3.10
 
 COPY entrypoint.sh /entrypoint.sh
 
-RUN apk add --no-cache git nodejs npm
+RUN apk add --no-cache git
 RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ echo "${REPOSITORY_FULL_NAME}"
 echo "${PR_DEPLOY_LABEL}"
 echo "${MAIN_BRANCH_NAME}"
 
-node index.js \
-  --r "${REPOSITORY_FULL_NAME}" \
-  --m "${PR_DEPLOY_LABEL}" \
-  --mb "${MAIN_BRANCH_NAME}" \
-  --graph
+# node index.js \
+#   --r "${REPOSITORY_FULL_NAME}" \
+#   --m "${PR_DEPLOY_LABEL}" \
+#   --mb "${MAIN_BRANCH_NAME}" \
+#   --graph
 
 ls
