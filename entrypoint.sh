f010a875185ae07e2f65018327b5bbcdd1b63b95
Shpingalet007 <pavelvernigorov@gmail.com>
2022-09-16 16:37:44 +0200

    Add conflictor script run

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,10 @@ echo "${REPOSITORY_FULL_NAME}"
 echo "${PR_DEPLOY_LABEL}"
 echo "${MAIN_BRANCH_NAME}"
 
-git branch
+node index.js \
+  --r "${REPOSITORY_FULL_NAME}" \
+  --m "${PR_DEPLOY_LABEL}" \
+  --mb "${MAIN_BRANCH_NAME}" \
+  --graph
+
+ls
