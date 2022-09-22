f39fb2468d6c2537a49a982d34efcf297a4eaafa
Shpingalet007 <22795961+shpingalet007@users.noreply.github.com>
2022-09-22 15:55:59 +0200

    Fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-# node index.js \
-#   --r "${REPOSITORY_FULL_NAME}" \
-#   --m "${PR_DEPLOY_LABEL}" \
-#   --mb "${MAIN_BRANCH_NAME}" \
-#   --graph
+node index.js \
+  --r "${REPOSITORY_FULL_NAME}" \
+  --m "${PR_DEPLOY_LABEL}" \
+  --mb "${MAIN_BRANCH_NAME}" \
+  --graph
